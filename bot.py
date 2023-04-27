d2a8e988afac8d209f6e0148d2ee66c1972ad5f6
Techwrdd <107527532+Techwrdd@users.noreply.github.com>
2023-04-27 16:43:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -93,7 +93,7 @@ async def op(_, m: Message):
                 ]
             )
             add_user(m.from_user.id)
-            await m.reply_photo("https://telegra.ph/file/12a39eecaa3031e3c4409.png", caption="**👋 Hello {}!\nI'm an auto approve Admin Join Requests Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @robotech_bots__**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
+            await m.reply_photo("https://telegra.ph/file/12a39eecaa3031e3c4409.png", caption="**👋 Hello {}!\nAdd This Bot To Your Channel To Accept Join Requests Automatically 😊\n\n__Powerd By : @robotech_bots__**".format(m.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard)
 
         elif m.chat.type == enums.ChatType.GROUP or enums.ChatType.SUPERGROUP:
             keyboar = InlineKeyboardMarkup(
@@ -140,7 +140,7 @@ async def chk(_, cb: CallbackQuery):
                 ]
             )
             add_user(cb.from_user.id)
-            await cb.message.edit("**👋 Hello {}!\nI'm an auto approve Admin Join Requests Bot.\nI can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.\n\n__Powerd By : @robotech_bots__**".format(cb.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard, disable_web_page_preview=True)
+            await cb.message.edit("**👋 Hello {}!\nAdd This Bot To Your Channel To Accept Join Requests Automatically 😊\n\n__Powerd By : @robotech_bots__**".format(cb.from_user.mention, "https://t.me/telegram/153"), reply_markup=keyboard, disable_web_page_preview=True)
         print(cb.from_user.first_name + " Is started Your Bot!")
     except UserNotParticipant:
         await cb.answer("🙅‍♂️ You are not joined to channel join and try again. 🙅‍♂️")
