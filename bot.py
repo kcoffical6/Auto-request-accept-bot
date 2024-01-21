439055835efc86b7a908984147162c728bba5454
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2024-01-21 08:26:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,7 +64,8 @@ async def approve(_, m: Message):
         add_group(m.chat.id)
         await app.approve_chat_join_request(op.id, kk.id)
         img = random.choice(gif)
-        await app.send_video(kk.id, img, "**Hello {}😈💜!\nYour request to join🤩 channel {} has been approved✅.keeps sharing and support us🫠🥹\n\n__Powerd By : #TEAM_APZ 😎🔥**".format(m.from_user.mention, m.chat.title))
+        btn=[[InlineKeyboardButton('movies', url='https://t.me/+sDor2v9aRzRiZmY1')]]
+        await app.send_video(kk.id, img, "**Hello {}😈💜!\nYour request to join🤩 channel {} has been approved✅.keeps sharing and support us🫠🥹\n\n__Powerd By : #TEAM_APZ 😎🔥**".format(m.from_user.mention, m.chat.title), reply_markup=InlineKeyboardMarkup(btn))
         add_user(kk.id)
     except errors.PeerIdInvalid as e:
         print("user isn't start bot(means group)")
