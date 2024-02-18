4da05d7cfb54e41ebcc53a7efce96fc0f42dd034
kcoffical6 <99400211+kcoffical6@users.noreply.github.com>
2024-02-18 21:42:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,13 +1,13 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "10079689"))
-    API_HASH = getenv("API_HASH", "8e402fe63a69576b82cda6daf1ab617f")
-    BOT_TOKEN = getenv("BOT_TOKEN", "6115136533:AAGTdrepiD0tnHCVKxhyV6nQIoJiC9qQlac")
-    FSUB = getenv("FSUB", "apz_botz")
-    CHID = int(getenv("CHID", "-1001727782613"))#update channel
+    API_ID = int(getenv("API_ID", "1669750"))
+    API_HASH = getenv("API_HASH", , "0f53ee8c576281995d621194aec588d8")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6651965209:AAFOrGxxekxbcgX7yB1BGYogodvJz5zXvEQ")
+    FSUB = getenv("FSUB", "")
+    CHID = int(getenv("CHID", "-1001755025897))#update channel
     SUDO = list(map(int, getenv("SUDO", "1729415252").split()))
-    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://Testing01:Testing01@cluster0.rstjt.mongodb.net/?retryWrites=true&w=majority")
-    WEB_SERVER = getenv("WEB_SERVER", True)
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://sudoscreen:5kEOBvPKpRoCCfyy@cluster0.swh5syh.mongodb.net/?retryWrites=true&w=majority")
+    WEB_SERVER = getenv("WEB_SERVER", False)
     PORT = int(getenv("PORT", 8080))
 cfg = Config()
